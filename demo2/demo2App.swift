5d3eba4a5db6c9f29eb448a76506c9a42bb74f4c
jeff-ji <68872443@qq.com>
2021-08-30 21:22:29 +0800

    Initial Commit

---
+++ demo2/demo2App.swift
@@ -0,0 +1,17 @@
+//
+//  demo2App.swift
+//  demo2
+//
+//  Created by jeff ji on 2021/8/30.
+//
+
+import SwiftUI
+
+@main
+struct demo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
